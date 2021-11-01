450c33a2160ba725460db21bd0ccdd5f5e53019d
fzhu886 <71693974+fzhu886@users.noreply.github.com>
2021-10-31 22:34:17 -0600

    Update CustomList.java
    Update CustomList

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -14,7 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.core.widget.TextViewCompat;
 
 import org.junit.Before;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
