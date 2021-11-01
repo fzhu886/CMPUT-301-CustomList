99b28490ec280301fd021c843689c086d7719fa2
fzhu886 <71693974+fzhu886@users.noreply.github.com>
2021-11-01 11:06:11 -0600

    Update CustomListTest.java

--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -22,4 +22,36 @@ public class CustomListTest {
         list.addCity(new City("Halifax", "NS"));
         assertEquals(list.getCount(), listSize+1);
     }
+    
+    @org.junit.Test
+    public void testHasCity() {
+        createList();
+        Assertions.assertEquals(0, list.getCount());
+
+        City city = new City("Regina", "Saskatchewan");
+        assertFalse(list.hasCity(city));
+        assertTrue(list.hasCity(city);
+    }
+
+    @org.junit.Test
+    public void testDelete() {
+        createList();
+        City city = new City("Regina", "Saskatchewan");
+        list.add(city);
+        Assertions.assertEquals(1, list.getCount());
+        list.delete(city);
+        Assertions.assertEquals(0, list.getCount());
+        assertFalse(list.hasCity(city));
+
+    }
+
+    @org.junit.Test
+    public void testCountCities() {
+        createList();
+        City city = new City("Regina", "Saskatchewan");
+        list.add(city);
+        Assertions.assertEquals(1, list.countCities());
+        list.delete(city);
+        Assertions.assertEquals(0, list.countCities());
+    }
 }
